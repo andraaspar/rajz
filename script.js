87672156669ef87c3217ff0be945d812f9c7e6bf
andraaspar <andraaspar@gmail.com>
2016-09-08 18:33:12 +0200

    Implemented timing & more tiles.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const CHAR_TO_ID = {
 	'N': 'ceiling-down',
 	'Z': 'ceiling-up',
 	' ': 'empty',
-	'#': 'full',
+	'%': 'full',
 	'L': 'ramp-down',
 	'J': 'ramp-up',
 	'*': 'star',
+	'#': 'checkers',
+	'0': 'dot',
 }
 const IMAGE = document.querySelector('pre')
 const R = render()
@@ -17,15 +19,19 @@ NEXT()
 setInterval(NEXT, TIME)
 
 function* render() {
-	for (let i = 0; true; i++) {
-		i = i % FRAMES.length
-		let chars = FRAMES[i]
+	for (let frameIndex = 0; true; frameIndex+=2) {
+		frameIndex = frameIndex % FRAMES.length
+		let time = FRAMES[frameIndex]
+		let chars = FRAMES[frameIndex + 1]
 		let html = ''
 		for (let char of chars.replace(/^\n|\n$/, '').split('')) {
 			if (char === '\n') html += char
 			else html += `<svg class="icon"><use xlink:href="#${CHAR_TO_ID[char]}"></use></svg>`
 		}
 		IMAGE.innerHTML = html
-		yield 0
+		
+		for (let timeIndex = 0; timeIndex < time; timeIndex++) {
+			yield
+		}
 	}
 }
